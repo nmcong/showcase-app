99e7e3a55f27d06665c95f66e8becb70e88a365f
Nguyen Cong <nmcong.it@gmail.com>
2025-11-22 11:07:49 +0700

    Fix: Update PresentationControls snap prop to boolean

--- src/components/3d/ModelViewer.tsx
+++ src/components/3d/ModelViewer.tsx
@@ -40,7 +40,7 @@ export function ModelViewer({ modelUrl, className = '' }: ModelViewerProps) {
           <PresentationControls
             global
             config={{ mass: 2, tension: 500 }}
-            snap={{ mass: 4, tension: 1500 }}
+            snap={true}
             rotation={[0, 0, 0]}
             polar={[-Math.PI / 3, Math.PI / 3]}
             azimuth={[-Math.PI / 1.4, Math.PI / 2]}
